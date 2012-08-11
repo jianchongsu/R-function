e93d3e584366357bcba60fcd296e7b8497ae97b6
jianchong su <jianchongsu@gmail.com>
2012-08-11 14:17:31 +0800

    create by myself
    day() can calucate the days from you birth to now,and the weekday about
    your birthday every year

---
+++ day.R
@@ -0,0 +1,13 @@
+day=function(d1,d2)
+  {
+   d1=as.character(d1)
+   d2=as.character(d2)
+ D1=as.Date(d1,"%m/%d/%y")
+D2=as.Date(d2,"%m/%d/%y")
+abs(D2-D1)
+birth=as.Date(min(D1,D2),"%m/%d/%y")
+   now=as.Date(max(D1,D2),"%m/%d/%y")
+   births=seq(from=birth,to=now,by="1 year")
+   list(days=abs(D2-D1),birthweek=weekdays(births))
+
+   }
